f573732e9eb64d192cd64b1a10a020f01bff460a
immortal_sniper1 <38986383+immortal-sniper1@users.noreply.github.com>
2021-03-03 08:34:56 +0200

    c++

---
+++ C++/waspmote-ota-manager/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+string  decitit;
+int sizex, verss;
+
+int main()
+{
+  ofstream myfile;
+  myfile.open ("UPGRADE.TXT");
+  cout << "introduceti numele fisierului binar " << endl;
+  cin >> decitit;
+  myfile << "FILE:";
+  myfile << decitit;
+  myfile << "\n";
+  myfile << "PATH:/\n";
+  cout << "introduceti marimea fisierului binar in bytes" << endl;
+  cin >> sizex;
+  myfile << "SIZE:";
+  myfile << sizex;
+  myfile << "\n";
+
+  cout << "introduceti versiunea fisierului binar " << endl;
+  cin >> verss;
+  myfile << "VERSION:";
+  myfile << verss;
+  myfile << "\n";
+
+
+  myfile.close();
+
+
+
+
+
+
+
+
+
+
+
+  return 0;
+}
