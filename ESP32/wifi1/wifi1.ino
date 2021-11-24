945379f1e5cc8fd82bf8d9474d2a1f994c4b5bfb
immortal_sniper1 <38986383+immortal-sniper1@users.noreply.github.com>
2021-11-24 13:09:13 +0200

    kk

--- ESP32/wifi1/wifi1.ino
+++ ESP32/wifi1/wifi1.ino
@@ -1,45 +1,41 @@
-#include <cmsis_os.h>
-#include <croutine.h>
-#include <event_groups.h>
-#include <FreeRTOS.h>
-#include <FreeRTOSConfig.h>
-#include <FreeRTOSConfig_Default.h>
-#include <list.h>
-#include <message_buffer.h>
-#include <mpu_prototypes.h>
-#include <portmacro.h>
-#include <queue.h>
-#include <semphr.h>
-#include <stack_macros.h>
-#include <STM32FreeRTOS.h>
-#include <stream_buffer.h>
-#include <task.h>
-#include <timers.h>
+#include <Adafruit_MQTT.h>
+#include <Adafruit_MQTT_Client.h>
+#include <Adafruit_MQTT_FONA.h>
 
 
-#include <ArduinoMqttClient.h>
+
+
+
+
+
+
+
 
 
 
 
 
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
+//#include <ESP8266WiFiMulti.h>
 
-#include <ESP8266HTTPClient.h>
-#include <ESP8266httpUpdate.h>
+//#include <ESP8266HTTPClient.h>
+//#include <ESP8266httpUpdate.h>
 
 
 /* Set these to your desired credentials. */
 const char *ssid = "LANCOMBEIA"; //Enter your WIFI ssid
 const char *password = "beialancom"; //Enter your WIFI password
 
+char server = 'mqtt.beia-telemetrie.ro';
+int port = 1883;
+//const char  user = "";
+//const char pass = "";
 
 
 
 
-
-int pinn = D4;
+int pinn2 = D0;
+int pinn = D7;
 
 
 void setup()
@@ -47,7 +43,7 @@ void setup()
 
   pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
   pinMode(pinn , OUTPUT);     // Initialize the LED_BUILTIN pin as an output
-
+  pinMode(pinn2 , OUTPUT);
 
   Serial.begin(115200);
   Serial.println();
@@ -64,7 +60,29 @@ void setup()
 
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
+  digitalWrite(pinn2, HIGH);
+
+  Serial.print("Connecting MQTT: ");
+  //Adafruit_MQTT.Adafruit_MQTT( server,  port, user, pass );
+  Adafruit_MQTT.Adafruit_MQTT( server,  port );
+  
+  int cc = Adafruit_MQTT.connect();
+  Serial.print("Succes= ", cc);
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
 
 // the loop function runs over and over again forever
 void loop()
@@ -74,9 +92,13 @@ void loop()
   digitalWrite(pinn, LOW);   // Turn the LED on (Note that LOW is the voltage level
   // but actually the LED is on; this is because
   // it is active low on the ESP-01)
-  delay(1000);                      // Wait for a second
+  delay(500);                      // Wait for a second
   digitalWrite(pinn, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(2000);
+  delay(4000);
+
+
+
+
 
 
 
