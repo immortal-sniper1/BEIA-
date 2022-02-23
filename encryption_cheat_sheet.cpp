ebacef3beb93e2eb97be26dbf9bc2c0061423ace
immortal_sniper1 <38986383+immortal-sniper1@users.noreply.github.com>
2022-02-23 13:12:09 +0200

    Update encryption_cheat_sheet.cpp

--- encryption_cheat_sheet.cpp
+++ encryption_cheat_sheet.cpp
@@ -1,4 +1,4 @@
-AES encrypt
+//AES encrypt
 // lib
 #include "WaspAES.h"
 
@@ -36,22 +36,56 @@ USB.println(F("-------------------------"));
 AES.printMessage( encrypted_message, encrypted_length);
 USB.println(F("-------------------------"));
 
-\\EXEMPLU
+//NOTE:
+//4G
+error = _4G.sendFrameToMeshlium( host, port, frame.buffer, frame.length);
+//se inlocuieste cu :
+error = _4G.sendFrameToMeshlium( host, port, encrypted_message, encrypted_length);
 
-Current ASCII Frame:
-Length: 63
-Frame Type:  134
-frame (HEX): 3C3D3E86022334453245314345383139363233433632236E6F64655F3030312330235354523A746869735F69735F615F6D657373616765234241543A373823
-frame (STR): <=>†#4E2E1CE819623C62#node_001#0#STR:this_is_a_message#BAT:78#
-===============================
 
-2. Encrypting Frame
-AES Encrypted message: "7B9558D56F02CB5E105C35F0596DA867148D5360B5429230A1DC35189F4E80F6CB3C1641C25E1E55545D3548C1EF073DFAC17E116C851E29BF5671F3F9C6C184"
-AES Encrypted length:64
+
+//WIFI
+error = WIFI_PRO.sendFrameToMeshlium( type, host, port, frame.buffer, frame.length);
+//se inlocuieste cu :
+error = WIFI_PRO.sendFrameToMeshlium( type, host, port, encrypted_message, encrypted_length);
+
+//EXEMPLU
+
+//Current ASCII Frame:
+//Length: 63
+//Frame Type:  134
+//frame (HEX): 3C3D3E86022334453245314345383139363233433632236E6F64655F3030312330235354523A746869735F69735F615F6D657373616765234241543A373823
+//frame (STR): <=>†#4E2E1CE819623C62#node_001#0#STR:this_is_a_message#BAT:78#
+//===============================
+
+//2. Encrypting Frame
+//AES Encrypted message: "7B9558D56F02CB5E105C35F0596DA867148D5360B5429230A1DC35189F4E80F6CB3C1641C25E1E55545D3548C1EF073DFAC17E116C851E29BF5671F3F9C6C184"
+//AES Encrypted length:64
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-RSA encrypt
+//RSA encrypt
 // lib
 #include "WaspRSA.h"
 
@@ -143,27 +177,27 @@ USB.println(F("-------------------------"));
 
 //EXEMPLU
 
-===============================
-Current ASCII Frame:
-Length: 63
-Frame Type:  134
-frame (HEX): 3C3D3E8602233445324531434538313936323343363223574153505F656E63727970746564233230235354523A58426565206672616D65234241543A373823
-frame (STR): <=>†#4E2E1CE819623C62#WASP_encrypted#20#STR:XBee frame#BAT:78#
-===============================
+//===============================
+//Current ASCII Frame:
+//Length: 63
+//Frame Type:  134
+//frame (HEX): 3C3D3E8602233445324531434538313936323343363223574153505F656E63727970746564233230235354523A58426565206672616D65234241543A373823
+//frame (STR): <=>†#4E2E1CE819623C62#WASP_encrypted#20#STR:XBee frame#BAT:78#
+//===============================
 
-2. Encrypting Frame
--------------------------
-Encrypted message:
--------------------------
-6332860CE012B90F65583E61932C54B4
-A983B68F54138C24260CE5577A1B58FB
-C256544BE813F2ED9866711194B0F401
-D3A14B2A1F165740BC7B9C72C4250A01
-FAA3CF3DF3EC97E2D3ED0F6C0556EC77
-455758C50BFB8366D49E0CB596F4C7F3
-11C232AE6C3C772C888CECFDE4FF6A1B
-6EEADCE3EBE53B919F3BFB120797CA78
+//2. Encrypting Frame
+//-------------------------
+//Encrypted message:
+//-------------------------
+//6332860CE012B90F65583E61932C54B4
+//A983B68F54138C24260CE5577A1B58FB
+//C256544BE813F2ED9866711194B0F401
+//D3A14B2A1F165740BC7B9C72C4250A01
+//FAA3CF3DF3EC97E2D3ED0F6C0556EC77
+//455758C50BFB8366D49E0CB596F4C7F3
+//11C232AE6C3C772C888CECFDE4FF6A1B
+//6EEADCE3EBE53B919F3BFB120797CA78
 
--------------------------
+//-------------------------
 
 
