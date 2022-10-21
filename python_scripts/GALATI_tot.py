21139be9cbcc42542ebd86f2757369319dd7d742
immortal_sniper1 <38986383+immortal-sniper1@users.noreply.github.com>
2022-10-21 12:26:11 +0300

    Update GALATI_tot.py

--- python_scripts/GALATI_tot.py
+++ python_scripts/GALATI_tot.py
@@ -2,68 +2,169 @@ import serial
 import time
 import paho.mqtt.client as mqtt
 import json
+from pyais.stream import FileReaderStream
+import datetime
 
-serialPort = serial.Serial(    port="COM6", baudrate=4800, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
-serialPort2 = serial.Serial(    port="COM7", baudrate=38400, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
-
+DEBUGG = True
 
 mqttBroker = 'mqtt.beia-telemetrie.ro'
 port = 1883
-topic  = "training/vital5g/DST-2/depth"
-topic2 = "training/vital5g/galati_AIS"
+topicDEP  = "training/vital5g/DST-2/depth"
+topicAIS = "training/vital5g/galati_AIS"
 
 client_id = "NUC-navrom"
 client = mqtt.Client(client_id)
-client.connect(mqttBroker) 
+client.connect(mqttBroker)
 
-serialString = ""  # Used to hold data coming over UART
+print("\nI'm alive, DEBUGG = " + str(DEBUGG))
 
-# while(1):
-#     client.publish(topic, "77")
-#     time.sleep(30)
+if DEBUGG:
+    print("connected to Beia mqtt broker ok") 
+
+def GetAISData():
+    
+    current_time = datetime.datetime.now()
+    
+    if DEBUGG:
+        print("\nGetting AIS data...")
+    
+    AISmessage = "no data"
+    
+    #connecting to serial port for AIS data
+    try:
+        serialPortAIS = serial.Serial(    port="COM7", baudrate=38400, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+        
+        if DEBUGG:
+            print("   connected to serial port for AIS data")
+            
+        time.sleep(1)
+        
+        if serialPortAIS.in_waiting > 0:
+            
+            if DEBUGG:
+                print("   found data in buffer")
+                
+            # Read data out of the buffer until a carraige return / new line is found
+            serialStringAIS = serialPortAIS.readline()
+            AISmessage = serialStringAIS.d
+            ecode("Ascii")
+            
+            if DEBUGG:
+                print("   message: " + AISmessage)
+            
+            # Print the contents of the serial data
+            
+            try:
+                stor = str(current_time) + ": " + str(AISmessage)
+                f = open("aisdata.txt", "a")
+                f.write(stor)
+                f.close()
+                
+                if DEBUGG:
+                    print("   stored message locally"),
+                    
+            except:
+                if DEBUGG:
+                    print("   failed to store data locally") 
+        else:
+            if DEBUGG:
+                print("   no data in buffer")
+            
+    except:
+        if DEBUGG:
+            print("   failed to connect to serial port for AIS data")
+        
+    finally:
+            serialPortAIS.close()
+
+    return AISmessage
 
 
-while 1:
-    # Wait until there is data waiting in the serial buffer
-    if serialPort.in_waiting > 0:
-
-        # Read data out of the buffer until a carraige return / new line is found
-        serialString = serialPort.readline()
-
-        # Print the contents of the serial data
-        try:
-            print(serialString.decode("Ascii"))
-
-            # mqtt de bagat cam pe aici
-            lungg=len(serialString)
-            smgg=serialString[lungg-20:lungg-17]
-            smgg2=serialString[lungg-18:lungg-15]
-            #nn=int(smgg)
-            print( "smgg= ", end = ' ' )
-            print( smgg )
-            print( "smgg2= ", end = ' ' )
-            print(  smgg2 )
-            outputt= smgg.decode()
-            outputt2= smgg2.decode()
-            print(outputt[:1])
-            print(outputt2[:1])
-            #ss2='{"id":"ceva","value":' + int(outputt2) + '}'
-            myJSON_test=json.dumps({"id":"GalatiPonton1","value":outputt2 })
-            json.dumps(["apple", 731])
-            #print(ss2)
-            print(  "grwtwtwtwetwet-----------------------------" )
-            #if outputt[:1]!="f" or  outputt[:1].isnumeric()    :
-            #client.publish(topic, outputt2   )
-            client.publish(topic,  myJSON_test  )
-            print(  "upssss2" )
-
-            serialString2 = serialPort2.readline()
-            print(serialString2.decode("Ascii"))
-            client.publish(topic2,  serialString2  )
-
-
-
-            time.sleep(5)
-
-        except:
+def GetDEPData():
+    
+    current_time = datetime.datetime.now()
+    
+    if DEBUGG:
+        print("\nGetting depth data...")
+    
+    DepthMessage = -1
+    
+    #connecting to serial port for AIS data
+    try:
+        serialPortDEP = serial.Serial(    port="COM8", baudrate=4800, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+        
+        if DEBUGG:
+            print("   connected to serial port for depth data")
+        
+        time.sleep(1)
+        
+        if serialPortDEP.in_waiting > 0:
+            
+            if DEBUGG:
+                print("   found data in buffer")
+                
+            # Read data out of the buffer until a carraige return / new line is found
+            serialStringDEP = serialPortDEP.readline()
+            DEPmessage = serialStringDEP.decode("Ascii")
+            
+            if DEBUGG:
+             print("   message: " + DEPmessage)
+            
+            # Print the contents of the serial data
+            
+            try:
+                stor = str(current_time) + ": " + str(DEPmessage)
+                f = open("depthdata.txt", "a")
+                f.write(stor)
+                f.close()
+                
+                if DEBUGG:
+                    print("   stored message locally"),
+                
+            except:
+                if DEBUGG:
+                    print("   failed to store data locally") 
+                    
+        else:
+            if DEBUGG:
+                print("   no data in buffer")
+                 
+    except:
+        if DEBUGG:
+            print("   failed to connect to serial port for depth data")
+        
+    finally:
+        serialPortDEP.close()
+        
+    splitMessage = DEPmessage.split(',')
+    
+    try:
+        if splitMessage[0] == "$SDDPT":
+            DepthMessage = float(splitMessage[1])
+    except:
+        if DEBUGG:
+            print("Unknown message format")
+        else:
             pass
+
+    return DepthMessage
+
+while True:
+    print("\n-----------New loop for getting data-----------\n")
+    
+    AISData = GetAISData()
+    DepthData = GetDEPData()
+    
+    print("\nAIS data: " + AISData)
+    print("Depth data: " + str(DepthData))
+    
+    payloadAIS = AISData
+    client.publish(topicAIS, payloadAIS)
+    
+    DepthDict = {"id":"GalatiPonton1", "value":DepthData }
+    payloadDEP = json.dumps(DepthDict)
+    client.publish(topicDEP, payloadDEP)
+            
+    print("\n------------------End of loop------------------\n")
+    print("Waiting 10 seconds before new loop...")
+    time.sleep(10)
