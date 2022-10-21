a503c09ce2557ce3c35a6dcad41994a0e6657684
immortal_sniper1 <38986383+immortal-sniper1@users.noreply.github.com>
2022-10-21 11:49:17 +0300

    Create GALATI_tot copy.py

---
+++ python_scripts/GALATI_tot copy.py
@@ -0,0 +1,77 @@
+import serial
+import time
+import paho.mqtt.client as mqtt
+import json
+from pyais import decode
+
+
+
+serialPort = serial.Serial(    port="COM6", baudrate=4800, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+serialPort2 = serial.Serial(    port="COM7", baudrate=38400, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+
+
+mqttBroker = 'mqtt.beia-telemetrie.ro'
+port = 1883
+topic  = "training/vital5g/DST-2/depth"
+topic2 = "training/vital5g/galati_AIS"
+topic3 = "training/vital5g/galati_AIS_decoded"
+
+
+client_id = "NUC-navrom"
+client = mqtt.Client(client_id)
+client.connect(mqttBroker) 
+
+serialString = ""  # Used to hold data coming over UART
+
+# while(1):
+#     client.publish(topic, "77")
+#     time.sleep(30)
+
+
+while 1:
+    # Wait until there is data waiting in the serial buffer
+    if serialPort.in_waiting > 0:
+
+        # Read data out of the buffer until a carraige return / new line is found
+        serialString = serialPort.readline()
+
+        # Print the contents of the serial data
+        try:
+            print(serialString.decode("Ascii"))
+
+            # mqtt de bagat cam pe aici
+            lungg=len(serialString)
+            smgg=serialString[lungg-20:lungg-17]
+            smgg2=serialString[lungg-18:lungg-15]
+            #nn=int(smgg)
+            print( "smgg= ", end = ' ' )
+            print( smgg )
+            print( "smgg2= ", end = ' ' )
+            print(  smgg2 )
+            outputt= smgg.decode()
+            outputt2= smgg2.decode()
+            print(outputt[:1])
+            print(outputt2[:1])
+            #ss2='{"id":"ceva","value":' + int(outputt2) + '}'
+            myJSON_test=json.dumps({"id":"GalatiPonton1","value":outputt2 })
+            json.dumps(["apple", 731])
+            #print(ss2)
+            print(  "grwtwtwtwetwet-----------------------------" )
+            #if outputt[:1]!="f" or  outputt[:1].isnumeric()    :
+            #client.publish(topic, outputt2   )
+            client.publish(topic,  myJSON_test  )
+            print(  "upssss2" )
+
+            serialString2 = serialPort2.readline()
+            print(serialString2.decode("Ascii"))
+            client.publish(topic2,  serialString2  )
+            decodat_AIS=decode(serialString2)
+            print(decodat_AIS)
+            client.publish(topic3,  decodat_AIS  )
+
+
+
+            time.sleep(5)
+
+        except:
+            pass
